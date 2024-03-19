3442a6515e1678a35df96bc5a2521d160937a377
Nikitpolin <polinann080@gmail.com>
2024-03-19 15:44:05 +0000

    2

---
+++ SpaceBattle/SoftStop.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+using Hwdtech;
+
+namespace SpaceBattle;
+public class SoftStop : _ICommand.ICommand
+{
+    public ServerThread thread;
+    private readonly BlockingCollection<_ICommand.ICommand> queue;
+    public Action action = () => { };
+
+    public SoftStop(ServerThread thread, Action action, BlockingCollection<_ICommand.ICommand> queue)
+    {
+        this.thread = thread;
+        this.action = action;
+        this.queue = queue;
+    }
+    public void Execute()
+    {
+        if (thread.Equals(Thread.CurrentThread))
+        {
+            thread.UpdateBehavior(() =>
+            {
+                if (queue.TryTake(out var command) == true)
+                {
+                    var cmd = queue.Take();
+                    try
+                    {
+                        cmd.Execute();
+                    }
+                    catch (Exception e)
+                    {
+                        IoC.Resolve<_ICommand.ICommand>("ExceptionHandler.Handle", cmd, e).Execute();
+                    }
+                }
+                else
+                {
+                    thread.Stop();
+                    action();
+
+                }
+            });
+        }
+        else
+        {
+            throw new Exception("Wrong Thread");
+        }
+    }
+}
